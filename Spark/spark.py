0a541be99ea214479986be06d09e722ca8e36429
Sam Goldrup <sgoldrup3@gmail.com>
2023-03-12 20:20:01 -0600

    new labs

--- Spark/spark.py
+++ Spark/spark.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import VectorAssembler, StringIndexer, OneHotEncoder
 from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
-from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.classification import RandomForestClassifier, LogisticRegression
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator as MCE
 
 
@@ -25,8 +25,21 @@ def word_count(filename='huck_finn.txt'):
     Returns:
         word_counts (list): list of (word, count) pairs for the 20 most used words
     """ 
-    raise NotImplementedError("Problem 1 Incomplete")
-    
+    #open spark sesh
+    spark = SparkSession\
+            .builder\
+            .appName("app_name")\
+            .getOrCreate()
+
+    hf = spark.sparkContext.textFile(filename)
+    hf = hf.flatMap(lambda row: row.split())
+    hf = hf.map(lambda row: (row,1))
+    hf = hf.reduceByKey(lambda x,y: x + y)
+    hf = hf.sortBy(lambda row: row[1])
+    return_var = hf.sortBy(lambda row: row[1], ascending=False).collect()[:20] #first 20 most common words
+    spark.stop() #closed!
+
+    return return_var   
     
 ### Problem 2
 def monte_carlo(n=10**5, parts=6):
@@ -38,7 +51,21 @@ def monte_carlo(n=10**5, parts=6):
     Returns:
         pi_est (float): estimated value of pi
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    #open the spark session
+    spark = SparkSession\
+            .builder\
+            .appName("app_name")\
+            .getOrCreate()
+
+    #partition into parts partitions
+    pts = spark.sparkContext.parallelize(np.random.uniform([-1,-1],[1,1],size=(n*parts,2)),parts)
+    in_circle = pts.map(lambda row: 1 if np.linalg.norm(row,ord=2) <= 1 else 0) #norm <= 1
+    total_in_circle = in_circle.reduce(lambda x,y: x + y) #add up no. of pts in circle
+    spark.stop()
+
+    pi_est = 4*(total_in_circle / (n*parts)) #square has area 2*2=4
+
+    return pi_est
 
 
 # ------------------------------- DataFrames ------------------------------- #
@@ -52,7 +79,31 @@ def titanic_df(filename='titanic.csv'):
              the survival rate of women, 
              and the survival rate of men in that order.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    spark = SparkSession\
+          .builder\
+          .appName("app_name")\
+          .getOrCreate()
+
+    schema = ('survived INT, pclass INT, name STRING, sex STRING, age FLOAT, sibsp INT, parch INT, fare FLOAT')
+    titanic = spark.read.csv("titanic.csv",schema=schema)
+    survival = titanic.groupBy('sex','survived').count()
+
+    data = survival.collect()
+
+    spark.stop()
+
+    num_women, num_men = 0.,0. #start out with floats
+    num_women += sum([row['count'] for row in data if row['sex'] == 'female']) #filter by male, female
+    num_men += sum([row['count'] for row in data if row['sex'] == 'male'])
+    
+    #filter by male,female survivors
+    women_survive = sum([row['count'] for row in data if row['sex'] == 'female' and row['survived'] == 1])
+    women_survival_rate = women_survive / num_women
+    men_survive = sum([row['count'] for row in data if row['sex'] == 'male' and row['survived'] == 1])
+    men_survival_rate = men_survive / num_men
+
+    answer = (num_women, num_men, women_survival_rate, men_survival_rate)
+    return answer
 
 
 ### Problem 4
@@ -67,7 +118,38 @@ def crime_and_income(crimefile='london_crime_by_lsoa.csv',
     returns:
         numpy array: borough names sorted by percent months with crime, descending
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    spark = SparkSession\
+          .builder\
+          .appName("app_name")\
+          .getOrCreate()
+
+    #read in the framez
+    schema_income = ('borough STRING, mean FLOAT, median FLOAT')
+    income = spark.read.csv(incomefile,header = True, inferSchema = True)
+
+    schema_crime = ('lsoa_code STRING, borough STRING, major_category STRING, minor_category STRING, value INT, year INT, month INT')
+    crime = spark.read.csv(crimefile,header = True, inferSchema = True)
+
+    crime = crime.filter(crime.major_category == major_cat) #filter by major_cat
+    crime = crime.groupBy("borough").sum("value") #groupBy borough, adding total values
+
+    income_crime = crime.join(income, on='borough') #merge on borough name
+    income_crime = income_crime.select("borough", "sum(value)", "median-08-16") #keep these columns
+    income_crime = income_crime.sort("sum(value)", ascending=False) #sort by value size, descending order
+    income_crime = income_crime.withColumnRenamed("sum(value)", f"{major_cat}"+"_total_crime") #rename column
+
+    ic_arr = np.array(income_crime.collect()) #the final data array
+
+    spark.stop()
+
+    #visualization
+    plt.scatter(ic_arr[:,2].astype(float),ic_arr[:,1].astype(float))
+    plt.xlabel("Median Income (2008-2016)")
+    plt.ylabel("Total Crimes (2008-2016)")
+    plt.title(f"{major_cat}")
+    plt.show()
+
+    return ic_arr
 
 
 ### Problem 5
@@ -80,4 +162,50 @@ def titanic_classifier(filename='titanic.csv'):
         metrics (tuple): a tuple of metrics gauging the performance of the model
             ('accuracy', 'weightedRecall', 'weightedPrecision')
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    spark = SparkSession\
+          .builder\
+          .appName("app_name")\
+          .getOrCreate()
+
+    #set up the data problem
+    schema = ('survived INT, pclass INT, name STRING, sex STRING, ''age FLOAT, sibsp INT, parch INT, fare FLOAT')
+    titanic = spark.read.csv('titanic.csv', schema=schema)
+
+    sex_binary = StringIndexer(inputCol='sex', outputCol='sex_binary')
+    onehot = OneHotEncoder(inputCols=['pclass'], outputCols=['pclass_onehot'])
+
+    features = ['sex_binary', 'pclass_onehot', 'age', 'sibsp', 'parch', 'fare']
+    features_col = VectorAssembler(inputCols=features, outputCol='features')
+
+    pipeline = Pipeline(stages=[sex_binary, onehot, features_col])
+    titanic = pipeline.fit(titanic).transform(titanic)
+    titanic = titanic.drop('pclass', 'name', 'sex')
+    train, test = titanic.randomSplit([0.75, 0.25], seed=11)
+
+    #logistic regression classifier
+    lr = LogisticRegression(labelCol='survived', featuresCol='features')
+    paramGrid = ParamGridBuilder()\
+                    .addGrid(lr.elasticNetParam, [0, 0.5, 1]).build()
+
+    tvs = TrainValidationSplit(estimator=lr,estimatorParamMaps=paramGrid,evaluator=MCE(labelCol='survived'),trainRatio=0.75,seed=11)
+    clf = tvs.fit(train)
+    results = clf.bestModel.evaluate(test)
+    #results.predictions.select(['survived', 'prediction']).show(5)
+
+    #now we do with random forest classifier
+    rf = RandomForestClassifier(labelCol = 'survived', featuresCol = 'features')
+    paramGrid = ParamGridBuilder()\
+                .addGrid(rf.maxBins, [6, 3, 12]).build()
+
+    rf_tvs = TrainValidationSplit(estimator=rf, estimatorParamMaps=paramGrid, evaluator = MCE(labelCol='survived'),trainRatio=0.75,seed=11)
+
+
+    rf_clf = rf_tvs.fit(train)
+    results = rf_clf.bestModel.evaluate(test)
+    accuracy = results.accuracy
+    weightedRecall = results.weightedRecall
+    weightedPrecision = results.weightedPrecision
+
+    spark.stop()
+
+    return (accuracy, weightedRecall, weightedPrecision)
--- WebScraping/web_scraping.py
+++ WebScraping/web_scraping.py
@@ -1,8 +1,15 @@
 """Volume 3: Web Scraping.
-<Name>
-<Class>
-<Date>
+Samuel Goldrup
+Math 405
+23 January 2023
 """
+
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+import numpy as np
+from matplotlib import pyplot as plt
 
 # Problem 1
 def prob1():
@@ -11,7 +18,13 @@ def prob1():
     Save the source as a file called example.html.
     If the file already exists, do not scrape the website or overwrite the file.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    if not os.path.exists("./example.html"): #don't wanna overwrite that file
+        jeff_source = requests.get("https://www.example.com") #named after my best friend, Jeff
+        with open("example.html","w") as file:
+            file.write(jeff_source.text)
+
+    return None
+    
     
 # Problem 2
 def prob2(code):
@@ -20,8 +33,11 @@ def prob2(code):
         code (str): A string of html code
     Returns:
         (list): Names of all tags in the given code"""
-    raise NotImplementedError("Problem 2 Incomplete")
-
+    small_soup = BeautifulSoup(code,'html.parser')
+    tags = small_soup.find_all(True) #first find their names, then
+    tag_names = [tag.name for tag in tags] #get their names
+    return tag_names
+    
 
 # Problem 3
 def prob3(filename="example.html"):
@@ -34,7 +50,18 @@ def prob3(filename="example.html"):
         (str): text of first <a> tag
         (bool): whether or not the tag has an 'href' attribute
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    with open(filename,'r') as file:
+        code = file.read() #read in the html code
+    
+    example_soup = BeautifulSoup(code,'html.parser') #turn it into soup
+    first_a_tag = example_soup.a #get those tags
+    href = False
+    if hasattr(first_a_tag,"href"): #see if it has an href attribute
+        href = True
+
+    txt = first_a_tag.get_text() #this is the string
+    
+    return txt, href
 
 
 # Problem 4
@@ -50,7 +77,17 @@ def prob4(filename="san_diego_weather.html"):
     Returns:
         (list) A list of bs4.element.Tag objects (NOT text).
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    with open(filename,'r') as file:
+        code = file.read() #read in the HTML code
+
+    weather_soup = BeautifulSoup(code,'html.parser') #turn it in to soup
+
+    ans_1 = weather_soup.find(string="Thursday, January 1, 2015").parent #tag with the date
+    ans_2_1 = weather_soup.find(string=re.compile(r"Previous Day")).parent #previous/next day tags
+    ans_2_2 = weather_soup.find(string=re.compile(r"Next Day")).parent
+    ans_3 = weather_soup.find(string="59").parent #the absolute maximum temperature
+   
+    return [ans_1, ans_2_1, ans_2_2, ans_3] #return as a list
 
 
 # Problem 5
@@ -62,8 +99,15 @@ def prob5(filename="large_banks_index.html"):
     Returns:
         (list): A list of bs4.element.Tag objects (NOT text).
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    with open(filename,'r') as file:
+        code = file.read() #read in the HTML code
 
+    fed_soup = BeautifulSoup(code,'html.parser') #turn it into soup
+
+    year_elements = fed_soup.find_all(string=re.compile("20(0[3-9]|1[0-4])")) #get all the 2003-2014, filter off last two
+    year_tags = [y_el.parent for y_el in year_elements] #get their parents
+
+    return year_tags[:-2]
 
 # Problem 6
 def prob6(filename="large_banks_data.html"):
@@ -75,4 +119,87 @@ def prob6(filename="large_banks_data.html"):
 
     In the case of a tie, sort the banks alphabetically by name.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    with open(filename,'r') as file:
+        code = file.read() #read in HTML code
+
+
+    fed_soup = BeautifulSoup(code,'html.parser') #federal reserve soup
+    trs = fed_soup.find_all(name='tr')[5:1385] #get all tr tags
+
+    #get all data for name, # of domestic banks, # of foreign banks
+    names = np.array([tr.get_text().split('\n')[1] if tr.get_text() != None else None for tr in trs])
+    doms = [tr.get_text().split('\n')[10] if tr.get_text() != None else None for tr in trs]
+    foreigns = [tr.get_text().split('\n')[11] if tr.get_text() != None else None for tr in trs]
+
+    #filter out the crap
+    for i in range(len(doms)):
+        doms[i] = doms[i].replace(",","")
+        if "." in doms[i]:
+            doms[i] = '0'
+
+    for i in range(len(foreigns)):
+        foreigns[i] = foreigns[i].replace(',',"")
+        if "." in foreigns[i]:
+            foreigns[i] = '0'
+
+    #turn into floats yay
+    doms = np.array(doms).astype(float)
+    foreigns = np.array(foreigns).astype(float)
+
+    #isolate the top seven
+    ords_dom = np.argsort(doms)[::-1]
+    ords_foreign = np.argsort(foreigns)[::-1]
+
+    data_dom = doms[ords_dom][:7]
+    labels_dom = names[ords_dom][:7]
+
+    data_foreign = foreigns[ords_foreign][:7]
+    labels_foreign = names[ords_foreign][:7]
+
+    #now plot horizontal bar charts
+
+    plt.subplot(1,2,1)
+    plt.barh(labels_dom,data_dom)
+    plt.yticks(fontsize=5)
+    plt.title("Domestic Branches")
+
+    plt.subplot(1,2,2)
+    plt.barh(labels_foreign,data_foreign)
+    plt.yticks(fontsize=5)
+    plt.title("Foreign Branches")
+
+    plt.suptitle("Federal Reserve Bank Analysis")
+    plt.tight_layout()
+    plt.show()
+
+
+def test_1():
+    print(prob1())
+
+def test_2():
+    small_example_html = """
+    <html><body><p>
+    Click <a id='info' href='http://www.example.com'>here</a> for more information.
+    </p></body></html>
+    """
+    print(prob2(small_example_html))
+
+def test_3():
+    print(prob3())
+
+def test_4():
+    print(prob4())
+
+def test_5():
+    print(prob5())
+
+def test_6():
+    print(prob6())
+
+def test_all():
+    test_1()
+    test_2()
+    test_3()
+    test_4()
+    test_5()
+    test_6()
